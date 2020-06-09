4b54c62b48c659c9f3789e34ba9a3a984f50292f
tglane <timo.glane@gmail.com>
2020-06-09 19:47:38 +0200

    refactored project structure

--- os_one/src/kernel/kmain.cc
+++ os_one/src/kernel/kmain.cc
@@ -6,10 +6,12 @@ T do_stuff(T&& t)
     return t;
 }
 
-extern "C" void main()
+extern "C" int main()
 {
     uint16_t* video_buffer = (uint16_t*) 0xb8000;
     uint8_t color = 15 | 0 << 4;
     video_buffer[0] = (uint16_t) do_stuff<unsigned char>('f') | (uint16_t) color << 8;
+
+    return 0;
 }
 
